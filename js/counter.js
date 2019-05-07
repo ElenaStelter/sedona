a7dbe2f172a8c0b1e65c5aa402bccd1ec112b9c0
ElenaStelter <46948550+ElenaStelter@users.noreply.github.com>
2019-05-07 09:34:03 +0700

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,23 @@
+﻿$(function() {
+		  (function quantity() {
+			var $minus = $(".form-block__minus");
+			var $plus = $(".form-block__plus");
+
+			$minus.click(quantityMinus);
+			$plus.click(quantityPlus);
+
+			function quantityMinus(event) {
+			  event.preventDefault();
+			  var $number = $(this).siblings('.form-block__number');
+			  if ($number.val() > 0) {
+				$number.val(+$number.val() - 1);
+			  }
+			}
+
+			function quantityPlus(event) {
+			  event.preventDefault();
+			  var $number = $(this).siblings('.form-block__number');
+			  $number.val(+$number.val() + 1);
+			}
+		  })();
+		});
